09929a736ae385a00f94d551f79803221e9d77f9
Ace <acekiron@outlook.com>
2023-12-25 09:00:25 +0100

    Add plugin.video.fireplace v0.1.1

--- addons/plugin.video.fireplace/default.py
+++ addons/plugin.video.fireplace/default.py
@@ -1,9 +1,17 @@
 from lib import Xenodi
+import random
 
 def ActionMainMenu(params):
     Xenodi.Debug(params)
 
-    Xenodi.Execute("RunPlugin(plugin://plugin.video.youtube/play/?video_id=L_LUpnjgPso)")
+    video_ids = [
+        "L_LUpnjgPso",
+        "g6Ye4xwXyAw",
+        "HF6LSbMKvrw",
+        "RrPP6NaF4gk"
+    ]
+
+    Xenodi.Execute("RunPlugin(plugin://plugin.video.youtube/play/?video_id=" + random.choice(video_ids) + "&incognito=true)")
 
 if __name__ == "__main__":
     Xenodi.Main(globals())
